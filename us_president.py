06b4fc8e44fb849ae31467536a48da5c1c27ba6a
Zi Qing Chua <ziqingchua@swuser-129-8-194-110.swlan.csufresno.edu>
2022-02-15 15:41:25 -0800

    first commit

---
+++ us_president.py
@@ -0,0 +1,12 @@
+## Display presidents with a specified first name.
+firstName = input ("Enter a first name: ")
+foundFlag = False
+infile = open("USPres.txt", 'r')
+for line in infile:
+    if line.startswith(firstName + ' '):
+        print (line.rstrip())
+        foundFlag = True
+if not foundFlag:
+    print ("No president had the first name",firstName + '.')
+    
+    
